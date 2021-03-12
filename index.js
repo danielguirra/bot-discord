63bbe890c8919cf913bd2fce897120db83585876
root <root@ip-172-31-11-19.us-east-2.compute.internal>
2021-03-12 11:13:37 +0000

    primeiro commit

---
+++ index.js
@@ -0,0 +1,123 @@
+const Discord = require('discord.js')
+require('dotenv').config()
+const bot = new Discord.Client()
+
+bot.login(process.env.BOTTOKEN)
+bot.on('ready', () => {
+    console.log('Vai')
+
+})
+
+bot.on('message', msg => {
+    const text = msg.content.toLowerCase().trim()
+    if (msg.content === 'Salve') {
+        msg.reply('Auuuuuuuuuuuuuuuuuuuuuu:wolf::wolf::wolf::wolf::wolf::wolf::wolf::wolf:')
+    }
+    if (msg.content === 'hora' || msg.content === 'Hora') {
+        var data = new Date();
+        var dia = data.getDate()           // 1-31
+        var dia_sem = data.getDay()            // 0-6 (zero=domingo)
+        var mes = data.getMonth()          // 0-11 (zero=janeiro)
+        var ano2 = data.getYear()           // 2 dígitos
+        var ano4 = data.getFullYear()       // 4 dígitos
+        var hora = data.getHours()        // 0-23
+        var min = data.getMinutes()        // 0-59
+        var seg = data.getSeconds()        // 0-59
+        var mseg = data.getMilliseconds()   // 0-999
+        var tz = data.getTimezoneOffset() // em minutos
+
+        // Formata a data e a hora (note o mês + 1)
+        var str_data = dia + '/' + (mes + 1) + '/' + ano4;
+        var str_hora = hora + ':' + min + ':' + seg;
+
+        // Mostra o resultado
+        msg.reply(':wolf::wolf::wolf::wolf::wolf::wolf::wolf::wolf:Hoje é ' + str_data + ' às ' + str_hora + ':wolf::wolf::wolf::wolf::wolf::wolf::wolf::wolf:')
+    }
+    if (msg.content === 'Avatar' || msg.content === 'avatar') {
+        msg.reply(`O seu avatar é esse::wolf::wolf::wolf::wolf:\n` + msg.author.displayAvatarURL())
+    }
+    if (msg.content === 'naruto') {
+        msg.reply('-p https://www.youtube.com/watch?v=Xc7lUbWEbqY&list=RDXc7lUbWEbqY')
+    }
+    if (msg.content === 'alok') {
+        msg.reply('-p https://open.spotify.com/playlist/37i9dQZF1DWZUWZIwpqsT3#_=_')
+    }
+    if (msg.content === `Servidor` || msg.content === 'servidor') {
+        msg.channel.send(`Nome do Serv: ${msg.guild.name}\nTotal de Animais: ${msg.guild.memberCount}:wolf:`);
+    }
+    if (msg.content === `usuario`) {
+        msg.channel.send(`Seu Nome de Usuário: ${msg.author.username}\nSeu ID: ${msg.author.id}:wolf:`);
+    }
+    if (text.includes('-cargo')) {
+        const rText = text.replace('-cargo', '').trim()
+        if (rText === 'games') {
+            var role = msg.member.guild.roles.cache.find(role => role.id === "818235836206153768")
+            msg.member.roles.add(role)
+            msg.reply(`Seu cargo foi atualizado com liberação de Gamer, boa partida até\n:wolf::wolf:`)
+        }
+        else if (rText === 'dev') {
+            var role = msg.member.guild.roles.cache.find(role => role.id === "818235920448487464")
+            msg.member.roles.add(role)
+            msg.reply(`Seu cargo foi atualizado com liberação de Dev, bom estudo até\n:wolf::wolf:`)
+        }
+        else {
+            const informacoes = msg.member.guild.channels.cache.find((channel) => channel.name.includes('informações'))
+            msg.reply(`:wolf:Por favor verique se digitou certo o cargo de desejado no ${informacoes}:wolf:`)
+        }
+    }
+
+})
+
+bot.on('guildMemberAdd', (member) => {
+
+    console.log(member)
+
+
+    const channel = member.guild.channels.cache.find((channel) => channel.name.includes('bem-vindo'))
+    const regras = member.guild.channels.cache.find((channel) => channel.name.includes('regras'))
+
+
+    const message = `:wolf:Bem vindo <@${member.id}> ao servidor,\n  Por favor verificar nossas ${regras}:wolf:`
+
+    channel.send(message)
+    var role = member.guild.roles.cache.find(role => role.id === "707012360367505480")
+    member.roles.add(role)
+
+})
+
+
+/*bot.on('ban', (message) => {
+    const { member, metions } = message
+
+    const tag = `<@${member.id}>`
+
+    if (
+        member.hasPermission('ADMINISTRATOR') ||
+        member.hasPermission('BAN_MEMBERS')
+    ) {
+        const target = mentions.users.first()
+        if (target) {
+            const targetMember = message.guild.members.chache.get(target.id)
+            targetMember.ban()
+            message.channel.send(`${tag}, Ta banido.`)
+        } else {
+            message.channel.send(`${tag}, Escreve certo por favor`)
+        }
+
+    } else {
+        message.channel.send(`${tag}, Você não pode banir ele`)
+    }
+})
+bot.on(['cc','limpa'], (msg) => {
+    if(msg.content==='cc'){
+        if (msg.member.hasPermission('ADMINISTRATOR')) {
+         msg.channel.msg.fetch().then((results) => {
+               msg.channel.bulkDelete(results)
+            })
+        }
+
+    }
+}
+)*/
+
+
