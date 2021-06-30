c20d57aadc38adf011c09db28d662545650570f9
danielguirra <danielsttt@hotmail.com.br>
2021-06-30 11:37:08 -0300

    on-kick-member

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ require("./service/on-ready-controller");
 require("./service/on-music");
 require("./service/on-guild-member-add-controller");
 require("./service/on-message-controller");
+require("./service/on-kick-member")
--- src/service/on-kick-member.js
+++ src/service/on-kick-member.js
@@ -1,11 +1,5 @@
-function getEmbed(title, description) {
-    let embed = new Discord.MessageEmbed()
-        .setColor("#6c856f")
-        .setTitle(title)
-        .setDescription(description);
 
-    return embed;
-}
+console.log('Ué')
 global.bot.on('message', message => {
     if (!message.guild) return;
     if (message.content.startsWith('*kick')) {
@@ -19,14 +13,22 @@ global.bot.on('message', message => {
                         message.channel.send(getEmbed('Expulso', `${user.tag}`));
                     })
                     .catch(err => {
-                        message.channel.send(getEmbed('Não posso expulsar esse membro'))
+                        message.channel.send(getEmbed('ERRO', 'Não posso expulsar'))
                         console.error(err);
                     });
             } else {
-                message.channel.send(getEmbed("Ele tem que estar no servidor!"));
+                message.channel.send(getEmbed('ERRO', "Ele tem que estar no servidor!"));
             }
         } else {
-            message.channel.send(getEmbed("Você não mencionou o usuário para expulsar!"));
+            message.channel.send(getEmbed('ERRO', "Você não mencionou o usuário para expulsar!"));
         }
     }
 });
+function getEmbed(title, description) {
+    let embed = new Discord.MessageEmbed()
+        .setColor("#6c856f")
+        .setTitle(title)
+        .setDescription(description);
+
+    return embed;
+}
