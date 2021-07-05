149e364beeac89e64971dffc9b2a571480a8d3a9
danielguirra <danielsttt@hotmail.com.br>
2021-07-05 09:03:57 -0300

    commit-bat

--- src/service/on-message-controller.js
+++ src/service/on-message-controller.js
@@ -4,7 +4,7 @@ const diceController = require("../controllers/dice-controller");
 const roleemoji = require("./on-embed-role-manager");
 const fetch = require('node-fetch');
 const prefix = "*";
-const images = require('./images')
+const images = require('./images');
 
 function getEmbed(title, description) {
   let embed = new Discord.MessageEmbed()
@@ -13,6 +13,8 @@ function getEmbed(title, description) {
     .setDescription(description);
   return embed;
 }
+
+
 global.bot.on("message", async (msg) => {
   if (msg.author.id !== msg.client.user.id) {
     const text = msg.content.toLowerCase().trim();
@@ -43,28 +45,26 @@ global.bot.on("message", async (msg) => {
       msg.channel.send(json.results[random].url);
     }
 
-    if (text === prefix + "aviso") {
+    if (text.content = prefix + "aviso") {
+      const rText = text.replace("*aviso", " ").trim();
       msg.delete();
       msg.channel.send("@everyone");
-      msg.channel.send(getEmbed("Importante", `@everyone Bláblá`)); //No bláblá coloque o recado lembre-se de usar no canal que quer enviar o aviso
+      msg.channel.send(getEmbed("Importante", `@everyone ${rText}`)); //No bláblá coloque o recado lembre-se de usar no canal que quer enviar o aviso
     }
 
     if (text === "ajuda") {
       helpController.help(msg);
     }
+
     if (text === "avatar") {
       msg.reply(msg.author.displayAvatarURL());
     }
 
-    if (text === "salve") {
-      msg.reply("Eae");
-    }
-
-    if (text.includes("*d")) {
+    if (text === prefix + 'd') {
       diceController.dice(msg); //Dice roll
     }
 
-    if (text.toLowerCase().startsWith("cls")) {
+    if (text === prefix + "cls") {
       //comando limpa chat
       const rText = text.replace("cls", "").trim();
       msg.delete();
