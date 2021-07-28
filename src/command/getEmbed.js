7a528400e7b7d94027423f5320ce0449c43882e3
danielguirra <danielsttt@hotmail.com.br>
2021-07-28 11:07:20 -0300

    update embed .setFooter

--- src/command/getEmbed.js
+++ src/command/getEmbed.js
@@ -1,10 +1,17 @@
 const Discord = require('discord.js')
 
-const getEmbed = (title, description) => {
+const getEmbed = (title, description, authorAvatarURL, nickname) => {
+    if (!authorAvatarURL) {
+        authorAvatarURL = 'https://cdn.discordapp.com/avatars/811255307673010246/e62838ac7e74bd5879f19ad254b457e7.png?size=2048'
+    }
+    if (!nickname) {
+        nickname = 'Capivara do TI'
+    }
     let embed = new Discord.MessageEmbed()
         .setColor("#6c856f")
         .setTitle(title)
-        .setDescription(description);
+        .setDescription(description)
+        .setFooter(nickname, authorAvatarURL)
     return embed;
 }
 
--- src/command/notice.js
+++ src/command/notice.js
@@ -3,10 +3,14 @@ const getEmbed = require('./getEmbed')
 module.exports = {
     name: 'aviso',
     discription: 'Notice send channel',
-    execute(message, args){
-            message.delete();
-            message.channel.send("@everyone");
-            message.channel.send(getEmbed.getEmbed("Importante", `@everyone ${args}`));
-          
+    execute(message, args) {
+        let avatar = message.author.displayAvatarURL()
+        let member = message.guild.member(message.author)
+        let nickname = member.displayName
+        message.delete();
+        message.channel.send("@everyone");
+        //message.channel.send(author)
+        message.channel.send(getEmbed.getEmbed("Importante", `@everyone ${args}`, avatar, nickname));
+
     }
 }
