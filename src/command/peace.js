66c364c4a3e91db35c43dc4a5dc1eb2c5b49e759
danielguirra <danielsttt@hotmail.com.br>
2021-08-03 15:01:48 -0300

    canvas

--- src/command/peace.js
+++ src/command/peace.js
@@ -3,51 +3,56 @@
 module.exports = {
     name: 'paz',
     async execute(message, args) {
-        const member = message.guild.member(message.author)
-        const Canvas = require('canvas')
-        const Discord = require("discord.js");
-        const { registerFont, createCanvas } = require('canvas')
-        registerFont('./fonts/comic.ttf', { family: 'Comic' })
-        async function canvas() {
+        if (message.content.startsWith('*paz')) {
+            const user = message.mentions.users.first();
+            if (user) {
+                const member = message.guild.member(user)
+                const Canvas = require('canvas')
+                const Discord = require("discord.js");
+                const { registerFont, createCanvas } = require('canvas')
+                registerFont('./fonts/comic.ttf', { family: 'Comic' })
+                async function canvas() {
 
-            const applyText = (canvas, text) => {
-                const context = canvas.getContext('2d');
-                let fontSize = 70;
+                    const applyText = (canvas, text) => {
+                        const context = canvas.getContext('2d');
+                        let fontSize = 70;
 
-                do {
-                    context.font = `${fontSize -= 10}px comic`;
-                } while (context.measureText(text).width > canvas.width - 300);
+                        do {
+                            context.font = `${fontSize -= 10}px comic`;
+                        } while (context.measureText(text).width > canvas.width - 300);
 
-                return context.font;
-            };
-            const canvas = Canvas.createCanvas(1000, 600)
-            const context = canvas.getContext("2d")
-            const background = await Canvas.loadImage('./images/gambull.png')
+                        return context.font;
+                    };
+                    const canvas = Canvas.createCanvas(1000, 600)
+                    const context = canvas.getContext("2d")
+                    const background = await Canvas.loadImage('./images/gambull.png')
 
-            context.drawImage(background, 0, 0, canvas.width, canvas.height);
+                    context.drawImage(background, 0, 0, canvas.width, canvas.height);
 
-            context.strokeStyle = '#74037b';
-            context.strokeRect(0, 0, canvas.width, canvas.height);
+                    context.strokeStyle = '#74037b';
+                    context.strokeRect(0, 0, canvas.width, canvas.height);
 
-            context.fillStyle = '#ffffff';
-            context.strokeStyle = '#000000'
+                    context.fillStyle = '#ffffff';
+                    context.strokeStyle = '#000000'
 
-            context.font = '200px comic';
+                    context.font = '200px comic';
 
-            context.fillText('Mó paz', canvas.width / 4.2, canvas.height / 2);
-            context.strokeText('Mó paz', canvas.width / 4.2, canvas.height / 2);
+                    context.fillText('Mó paz', canvas.width / 4.2, canvas.height / 2);
+                    context.strokeText('Mó paz', canvas.width / 4.2, canvas.height / 2);
 
-            context.font = applyText(canvas);
-
-
-            const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'png' }));
+                    context.font = applyText(canvas);
 
 
-            context.drawImage(avatar, 690, 290, 250, 250);
-            const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'paz-image.png')
+                    const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'png' }));
 
-            message.channel.send(attachment)
+
+                    context.drawImage(avatar, 690, 290, 250, 250);
+                    const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'paz-image.png')
+
+                    message.channel.send(attachment)
+                }
+                canvas()
+            }
         }
-        canvas()
     }
 }
