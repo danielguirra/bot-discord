3482d9327100d3e00e5839f5a78c7f5e743e6fb0
danielguirra <daniel.guirra777@gmail.com>
2021-08-16 08:18:47 -0300

    new command teste

--- src/command/teste.js
+++ src/command/teste.js
@@ -2,17 +2,11 @@ const getEmbed = require('./getEmbed')
 const wait = require('util').promisify(setTimeout)
 
 module.exports = {
-    
+
     name: "ping",
-    category: "info",
-    aliases: [],
     description: "Check Ping Bot",
-    args: false,
-    usage: "",
-    permission: [],
-    owner: false,
-   async execute(message, args) {
-        await wait(10000)
+    async execute(message, args) {
+        await wait(1000)
         message.channel.send(getEmbed.getEmbed(`Latência`, `Ping : **${message.client.ws.ping}**ms`));
     }
 }
