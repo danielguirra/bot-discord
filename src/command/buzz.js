4574d95fd88cb2a1361c410085bf8703ad2739e9
danielguirra <daniel.guirra777@gmail.com>
2021-08-17 08:32:16 -0300

    ot

--- src/command/buzz.js
+++ src/command/buzz.js
@@ -15,23 +15,15 @@ module.exports = {
                 const { registerFont, createCanvas } = require('canvas')
                 registerFont('./fonts/comic.ttf', { family: 'Comic' })
                 async function canvas() {
-
-                    const applyText = (canvas, text) => {
-                        const context = canvas.getContext('2d');
-                        let fontSize = 70;
-
-                        do {
-                            context.font = `${fontSize -= 10}px comic`;
-                        } while (context.measureText(text).width > canvas.width - 300);
-
-                        return context.font;
-                    };
                     const canvas = Canvas.createCanvas(500, 750)
                     const context = canvas.getContext("2d")
+
                     const background = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'png' }));
                     const background2 = await Canvas.loadImage(member2.user.displayAvatarURL({ format: 'png' }));
+
                     context.drawImage(background, 250, 0, 250, 250);
                     context.drawImage(background2, 0, 0, 250, 250);
+
                     context.fillStyle = '#ffffff';
                     context.strokeStyle = '#000000'
 
@@ -40,11 +32,9 @@ module.exports = {
                     context.fillText('Man tu é guei, cara?', 260, 200);
                     context.strokeText('Man tu é guei, cara?', 260, 200);
 
-
                     const buzz = await Canvas.loadImage('https://i.im.ge/2021/08/04/U9iPL.jpg');
-
-
                     context.drawImage(buzz, 0, 250, 500, 500);
+
                     const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'guei.png')
 
                     message.channel.send(attachment)
