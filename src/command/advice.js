0f110db9d46b334f29403941abce374c5cedbd21
danielguirra <daniel.guirra777@gmail.com>
2021-08-24 10:40:22 -0300

    advice

---
+++ src/command/advice.js
@@ -0,0 +1,25 @@
+
+const translate = require('@vitalets/google-translate-api');
+const getEmbed = require('./getEmbed')
+const fetch = require('node-fetch')
+module.exports = {
+    name: 'advice',
+    description: 'translate advice for portuguese',
+    async execute(message) {
+        const member = message.guild.member(message.author)
+        let url = `https://api.adviceslip.com/advice`
+        let response = await fetch(url);
+        let json = await response.json();
+        const textoJson = JSON.stringify(json)
+        const significado = JSON.parse(textoJson)
+        let text = significado['slip']['advice']
+        translate(`${text}`, { to: `pt` }).then(res => {
+            message.channel.send(getEmbed.getEmbed('Conselho',
+                `${res.text}
+                      `, member.user.displayAvatarURL(), message.author.tag))
+        }).catch(err => {
+            console.error(err);
+        });
+    }
+}
+
