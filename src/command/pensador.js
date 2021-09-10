53b69d0367d7baa4b7df79205931cd61c62fb9e1
danielguirra <daniel.guirra777@gmail.com>
2021-09-10 08:51:20 -0300

    pensador

--- src/command/pensador.js
+++ src/command/pensador.js
@@ -8,17 +8,17 @@ module.exports = {
     async execute(message) {
         pensador.getFromCollection().then(result => {
             const textoJson = JSON.stringify(result)
-            const significado = JSON.parse(textoJson)
-            gis(significado['author'], logResults);
+            const frase = JSON.parse(textoJson)
+            gis(frase['author'], logResults);
             async function logResults(error, results) {
                 if (error) {
                     console.log(error)
                 } else {
-                    return message.channel.send(getEmbed.getEmbed(`Frase de ${significado['author']}`
-                        , `${significado['message']}`, results[0].url, significado['author']))
+                    return message.channel.send(getEmbed.getEmbed(`Frase de ${frase['author']}`
+                        , `${frase['message']}`, results[0].url, frase['author']))
                 }
             }
-            console.log(significado)
+            console.log(frase)
 
         });
 
