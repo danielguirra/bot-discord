5283f0852d1acfa5d8e8ff25855a9dee68933bdb
danielguirra <daniel.guirra777@gmail.com>
2021-09-24 09:57:03 -0300

    port

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const express = require('express');
 const app = express();
 app.get('/', (req, res) => res.send('Servidor online!'));
 
-app.listen(() => console.log(`O bot está rodando na porta: http://localhost:${port}`));
+app.listen();
