c29972e1fe62c76eacff6d5ab1a4606fac7c2e58
danielguirra <daniel.guirra777@gmail.com>
2021-09-24 10:07:16 -0300

    port

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
 require("dotenv").config();
 require("./src/index");
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => res.send('Servidor online!'));
 
-app.listen();
+
