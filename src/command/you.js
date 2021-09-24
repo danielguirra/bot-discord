d6e461e7548a56d0037656f04806a22d9c5b0a7a
danielguirra <daniel.guirra777@gmail.com>
2021-09-24 11:34:20 -0300

    otimizantion

--- src/command/you.js
+++ src/command/you.js
@@ -14,11 +14,11 @@ module.exports = {
         const canvas = Canvas.createCanvas(400, 400)
         const context = canvas.getContext("2d")
         const background = await Canvas.loadImage('https://i.im.ge/2021/09/24/T3cfXF.png')
-        context.drawImage(background, -150, 0, 700, canvas.height);
+        context.drawImage(background, 0, 0, 700, canvas.height);
         const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'png' }));
         context.drawImage(avatar, 100, 40, 200, 200);
 
-        const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'you.png')
+        const attachment = new MessageAttachment(canvas.toBuffer(), 'you.png')
 
         message.channel.send(attachment)
     }
