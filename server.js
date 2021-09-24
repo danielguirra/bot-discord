57ec4e2347dc98813b9ec0ebd6386e818bee46bf
danielguirra <daniel.guirra777@gmail.com>
2021-09-24 10:11:35 -0300

    port

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 require("dotenv").config();
 require("./src/index");
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => res.send('Servidor online!'));
 
-
+app.listen(port, () => console.log(`O bot está rodando na porta: http://localhost:${port}`));
